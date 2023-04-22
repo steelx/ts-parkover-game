0639dd7d0e36f2fbd1dc39daaa1269089a1c6ed6
Ajinkya <ajinkyax@pm.me>
2023-04-22 12:34:12 +0530

    fix: applyForce correctly

--- src/CharacterInputController.ts
+++ src/CharacterInputController.ts
@@ -7,6 +7,8 @@ export default class CharacterInputController {
     camera: UniversalCamera;
     inputMap: any;
     time: float = 0;
+    private stopForceKey: string = "f";
+    private forceApplied: boolean = false;
 
     constructor(private character: Player, game: Game) {
         this.scene = game.scene;
@@ -26,53 +28,45 @@ export default class CharacterInputController {
                     this.inputMap[kbInfo.event.key] = false;
                     break;
             }
+
+            if (this.inputMap[this.stopForceKey]) { // X key to stop movement
+                this.character.physicsImpostor?.setLinearVelocity(Vector3.Zero());
+                this.character.physicsImpostor?.setAngularVelocity(Vector3.Zero());
+            }
         });
 
         let lastUpdateTime = performance.now();
         this.scene.onBeforeRenderObservable.add(() => {
             const currentTime = performance.now();
             const elapsedTime = currentTime - lastUpdateTime;
-            const moveSpeed = 0.005 * elapsedTime;
+            const moveSpeed = 0.01 * elapsedTime;
             const strafeJumpSpeed = 5;
             this.time += elapsedTime * 0.001;
 
+            // Apply force only while keys are pressed
+            this.forceApplied = false;
+
             if (this.inputMap["w"] || this.inputMap["W"]) {
-                // this.character.moveWithCollisions(this.camera.getDirection(new Vector3(0, 0, 1)).scale(moveSpeed));
-                this.character
-                    .physicsImpostor?.applyImpulse(
-                        this.camera.getDirection(new Vector3(0, 0, 1)).scale(moveSpeed),
-                        this.character.getAbsolutePosition()
-                    );
+                this.applyForce(this.camera.getDirection(new Vector3(0, 0, 1)).scale(moveSpeed));
             }
             if (this.inputMap["s"] || this.inputMap["S"]) {
-                // this.character.moveWithCollisions(this.camera.getDirection(new Vector3(0, 0, -1)).scale(moveSpeed));
-                this.character
-                    .physicsImpostor?.applyImpulse(
-                        this.camera.getDirection(new Vector3(0, 0, -1)).scale(moveSpeed),
-                        this.character.getAbsolutePosition()
-                    );
+                this.applyForce(this.camera.getDirection(new Vector3(0, 0, -1)).scale(moveSpeed));
             }
             if (this.inputMap["a"] || this.inputMap["A"]) {
-                // this.character.moveWithCollisions(this.camera.getDirection(new Vector3(-1, 0, 0)).scale(moveSpeed));
-                this.character
-                    .physicsImpostor?.applyImpulse(
-                        this.camera.getDirection(new Vector3(0, 0, 1)).scale(moveSpeed),
-                        this.character.getAbsolutePosition()
-                    );
+                this.applyForce(this.camera.getDirection(new Vector3(0, 0, 1)).scale(moveSpeed));
             }
             if (this.inputMap["d"] || this.inputMap["D"]) {
-                // this.character.moveWithCollisions(this.camera.getDirection(new Vector3(1, 0, 0)).scale(moveSpeed));
-                this.character
-                    .physicsImpostor?.applyImpulse(
-                        this.camera.getDirection(new Vector3(1, 0, 0)).scale(moveSpeed),
-                        this.character.getAbsolutePosition()
-                    );
+                this.applyForce(this.camera.getDirection(new Vector3(1, 0, 0)).scale(moveSpeed));
             }
 
             if (this.inputMap[" "]) { // Space key for strafe jump
                 const jumpImpulse = new Vector3(0, strafeJumpSpeed, 0);
                 this.character.physicsImpostor?.applyImpulse(jumpImpulse, this.character.getAbsolutePosition());
                 this.inputMap[" "] = false;
+            }
+
+            if (!this.forceApplied) {
+                this.character.moveDirection = Vector3.Zero();
             }
 
 
@@ -86,5 +80,13 @@ export default class CharacterInputController {
             this.character.moveDirection = Vector3.Zero();
             lastUpdateTime = currentTime;
         });
+    }
+
+    private applyForce(force: Vector3) {
+        if (this.character.physicsImpostor) {
+            this.character.physicsImpostor.applyImpulse(force, this.character.getAbsolutePosition());
+            this.character.moveDirection.addInPlace(force);
+            this.forceApplied = true;
+        }
     }
 }
